d97037b68cdc8424dfaf1d8d63bd4b9cd4575964
Jul0P <jules.parents@gmail.com>
2024-10-22 00:53:00 +0200

    rename folders

--- src/models/Devoir.cs
+++
@@ -1,8 +0,0 @@
-namespace discord_bot_csharp.Models;
-
-public class Devoir
-{
-    public string Matiere { get; set; }
-    public string Professeur { get; set; }
-    public string Description { get; set; }
-}
--- src/models/DevoirDate.cs
+++
@@ -1,7 +0,0 @@
-namespace discord_bot_csharp.Models;
-
-public class DevoirDate
-{
-    public string Date { get; set; }
-    public Dictionary<string, List<Devoir>> Devoirs { get; set; } = new Dictionary<string, List<Devoir>>();
-}
